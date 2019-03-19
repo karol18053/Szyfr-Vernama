ccfaf4f2b58ab51be22253b08b32631eebfbf524
karol18053 <karol18053@gmail.com>
2019-03-19 10:50:31 +0100

    Update BlumBlumShub.java

--- BlumBlumShub.java
+++ BlumBlumShub.java
@@ -5,45 +5,6 @@ import java.security.SecureRandom;
 import java.math.BigInteger;
 
 /**
- * The Blum-Blum-Shub random number generator.
- *
- * <p>
- * The Blum-Blum-Shub is a "cryptographically secure" random number
- * generator.  It has been proven that predicting the ouput
- * is equivalent to factoring <i>n</i>, a large integer generated
- * from two prime numbers.
- * </p>
- *
- * <p>
- * The Algorithm:
- * </p>
- * <ol>
- * <li>
- *  (setup) generate two secret prime numbers <i>p</i>, <i>q</i> such that
- *   <i>p</i> &ne; <i>q</i>, <i>p</i> &equiv; 3 mod 4, <i>q</i> &equiv; 3 mod 4.
- * </li>
- * <li> (setup) compute <i>n</i> = <i>pq</i>.  <i>n</i> can be re-used, but
- *   <i>p</i>, and <i>q</i> are secret and should be disposed of.</li>
- * <li> Generate a (secure) random seed <i>s</i> in the range [1, <i>n</i> -1]
- *   such that gcd(<i>s</i>, <i>n</i>) = 1.
- * <li> Compute <i>x</i> = <i>s</i><sup>2</sup> mod <i>n</i></li>
- * <li> Compute a single random bit with:
- *   <ol>
- *   <li> <i>x</i> = <i>x</i><sup>2</sup> mod <i>n</i></li>
- *   <li> return Least-Significant-Bit(<i>x</i>) (i.e. <i>x</i> & 1)</li>
- *   </ol>
- * Repeat as necessary.
- * </li>
- * </ol>
- *
- * <p>
- * The code originally appeared in <a href="http://modp.com/cida/"><i>Cryptography for
- * Internet and Database Applications </i>, Chapter 4, pages 174-177</a>
- * </p>
- * <p>
- * More details are in  the <a href="http://www.cacr.math.uwaterloo.ca/hac/"><i>Handbook of Applied Cryptography</i></a>,
- * <a href="http://www.cacr.math.uwaterloo.ca/hac/about/chap5.pdf">Section 5.5.2</a>
- * </p>
  *
  * @author Nick Galbreath -- nickg [at] modp [dot] com
  * @version 3 -- 06-Jul-2005
